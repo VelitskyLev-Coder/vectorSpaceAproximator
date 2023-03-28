abe0938ecf58165dcee78fb4a840286445be6c02
VelitskyLev-Coder <VB6WkqKmScYqE2y>
2023-03-29 00:05:37 +0300

    function aproximation

--- aproximator.py
+++ aproximator.py
@@ -4,6 +4,7 @@ from math import sqrt
 import copy
 import itertools
 import sympy as sp
+from scipy.integrate import quad
 
 TVectorType = TypeVar("TVectorType", bound='IVector')
 
@@ -57,11 +58,22 @@ class FiniteVectorSpace:
     def get_orthonormal_base(self) -> list[IVector]:
         orthonormal_base = list()
         orthonormal_base.append(self.base[0].get_normalized())
-        zero_vec = self.base[0] - self.base[0]
+        zero_vec = self.get_zero_vector()
         for i, b_vec in enumerate(self.base[1:], start=1):
             orthonormal_base.append(
-                (b_vec - sum([self.VEC_CLASS.inner(b_vec, a)*a for a in orthonormal_base], zero_vec)).get_normalized())
+                (b_vec - sum([self.VEC_CLASS.inner(b_vec, a) * a for a in orthonormal_base],
+                             zero_vec)).get_normalized())
         return orthonormal_base
+
+    def get_zero_vector(self):
+        return self.base[0] - self.base[0]
+
+    def get_best_approximation(self, vec: IVector) -> (IVector, float):
+        zero_vec = self.get_zero_vector()
+        orthonormal_base = self.get_orthonormal_base()
+        w_star = sum([self.VEC_CLASS.inner(vec, a)*a for a in orthonormal_base], zero_vec)
+        min_dist = (vec-w_star).norm()
+        return w_star, min_dist
 
 
 class RNVec(IVector['RNVec']):
@@ -71,57 +83,64 @@ class RNVec(IVector['RNVec']):
         self.coord = list(coord)
 
     def __mul__(self, scalar: float) -> 'RNVec':
-        return RNVec(*[x*scalar for x in self.coord])
+        return RNVec(*[x * scalar for x in self.coord])
 
     def __add__(self, other: 'RNVec') -> 'RNVec':
-        return RNVec(*[x+y for x, y in zip(self.coord, other.coord)])
+        return RNVec(*[x + y for x, y in zip(self.coord, other.coord)])
 
     @staticmethod
     def inner(first: 'RNVec', second: 'RNVec') -> float:
-        return sum([x*y for x, y in zip(first.coord, second.coord)])
+        return sum([x * y for x, y in zip(first.coord, second.coord)])
 
     def __str__(self):
         return f'({", ".join(map(str, self.coord))})'
 
 
-F = TypeVar('F', bound=Callable[..., sp.Basic])
-
-class SimbolicFunction(IVector['SimbolicFunction']):
-    def __init__(self, func: F, ver: Any):
+class SymbolicFunction(IVector['SymbolicFunction']):
+    def __init__(self, func: Any, ver: Any):
         super().__init__()
         self.func = func
         self.ver = ver
 
-    def __mul__(self, scalar: float) -> 'SimbolicFunction':
-        return scalar*self.func
+    def __mul__(self, scalar: float) -> 'SymbolicFunction':
+        return SymbolicFunction(scalar * self.func, self.ver)
 
-    def __add__(self, other: 'SimbolicFunction') -> 'SimbolicFunction':
-        return self.func+other.func
+    def __add__(self, other: 'SymbolicFunction') -> 'SymbolicFunction':
+        return SymbolicFunction(self.func + other.func, self.ver)
 
     @staticmethod
-    def inner(first: 'SimbolicFunction', second: 'SimbolicFunction') -> float:
-        print(first.func, second.func, sp.integrate((first.func-second.func)**2, (first.ver, 0, 1)))
-        return sp.integrate(first.func*second.func, (first.ver, 0, 1))
+    def inner(first: 'SymbolicFunction', second: 'SymbolicFunction') -> float:
+        # numerical integration, L2
+        result, _ = quad(sp.lambdify(first.ver, first.func*second.func, 'numpy'), 0, 1)
+        return result
 
 
 def main():
     a = RNVec(-1, 3, 1, 1)
     b = RNVec(6, -8, -2, -4)
     c = RNVec(6, 3, 6, -3)
-    #print(a)
-    #print(b)
-    #print(c)
     W = FiniteVectorSpace(a, b, c)
-    #print('\n'.join([str(vec) for vec in W.get_orthonormal_base()]))
     x = sp.symbols('x')
-    print((x-x+1)-(x-x+1))
-    base = (
-        SimbolicFunction(x-x+1, x),
-        SimbolicFunction(x, x),
-        SimbolicFunction(x**2, x),
-    )
-    W = FiniteVectorSpace(*base)
-    print('\n'.join([str(vec) for vec in W.get_orthonormal_base()]))
+    base = []
+    #for n in range(1, 33):
+    #    base.append(SymbolicFunction(sp.sin(2*sp.pi*sp.Integer(n)*x), x))
+    #    base.append(SymbolicFunction(sp.cos(2 * sp.pi * sp.Integer(n)*x), x))
+    base.append(SymbolicFunction(sp.Integer(1), x))
+    for n in range(1, 15):
+        base.append(SymbolicFunction(x**n, x))
+        W = FiniteVectorSpace(*base)
+        w_star, min_dist = W.get_best_approximation(SymbolicFunction(sp.exp(sp.exp(x)*x*0.3), x))
+        print(str(w_star.func))
+        print('f(x) = ' + sp.latex(w_star.func))
+        print(min_dist)
+        print('****************')
+    #base.append(SymbolicFunction(x, x))
+
+    #W = FiniteVectorSpace(*base)
+    #w_star, min_dist = W.get_best_approximation(SymbolicFunction(sp.exp(sp.sin(sp.Number(10)*x)), x))
+    #print(str(w_star.func))
+    #print('f(x) = ' + sp.latex(w_star.func))
+    #print(min_dist)
 
 if __name__ == '__main__':
     main()
