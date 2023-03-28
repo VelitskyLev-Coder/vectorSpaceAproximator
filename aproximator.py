60ccdf0b4d83922fe2523f5d207039550766b41e
VelitskyLev-Coder <VB6WkqKmScYqE2y>
2023-03-28 20:57:48 +0300

    initial commit

---
+++ aproximator.py
@@ -0,0 +1,127 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar, Union, Callable, Any
+from math import sqrt
+import copy
+import itertools
+import sympy as sp
+
+TVectorType = TypeVar("TVectorType", bound='IVector')
+
+
+class IVector(ABC, Generic[TVectorType]):
+    """
+        This class is an interface class to represent a vector, with defined inner product.
+        This implementation assumes the scalars over the real numbers field.
+        The scalar is represented by the float type.
+    """
+
+    @abstractmethod
+    def __mul__(self, scalar: float) -> 'TVectorType':
+        pass
+
+    @abstractmethod
+    def __add__(self, other: 'TVectorType') -> 'TVectorType':
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def inner(first: 'TVectorType', second: 'TVectorType') -> float:
+        pass
+
+    def __rmul__(self, scalar: float) -> 'TVectorType':
+        return self.__mul__(scalar)
+
+    def __neg__(self) -> 'TVectorType':
+        return self.__mul__(-1)
+
+    def __sub__(self, other) -> 'TVectorType':
+        return self + (-other)
+
+    def __truediv__(self, scalar: float) -> 'TVectorType':
+        return 1 / scalar * self
+
+    def norm(self) -> float:
+        return sqrt(self.inner(self, self))
+
+    def get_normalized(self) -> TVectorType:
+        return self / self.norm()
+
+
+class FiniteVectorSpace:
+    def __init__(self, *base: IVector):
+        # TODO consider to add the linear independence of the base check or to calculate the dim here
+        self.dim = len(base)
+        self.base: list[IVector] = list(map(copy.deepcopy, base))
+        self.VEC_CLASS = self.base[0].__class__
+
+    def get_orthonormal_base(self) -> list[IVector]:
+        orthonormal_base = list()
+        orthonormal_base.append(self.base[0].get_normalized())
+        zero_vec = self.base[0] - self.base[0]
+        for i, b_vec in enumerate(self.base[1:], start=1):
+            orthonormal_base.append(
+                (b_vec - sum([self.VEC_CLASS.inner(b_vec, a)*a for a in orthonormal_base], zero_vec)).get_normalized())
+        return orthonormal_base
+
+
+class RNVec(IVector['RNVec']):
+    def __init__(self, *coord: float):
+        super().__init__()
+        self.n = len(coord)
+        self.coord = list(coord)
+
+    def __mul__(self, scalar: float) -> 'RNVec':
+        return RNVec(*[x*scalar for x in self.coord])
+
+    def __add__(self, other: 'RNVec') -> 'RNVec':
+        return RNVec(*[x+y for x, y in zip(self.coord, other.coord)])
+
+    @staticmethod
+    def inner(first: 'RNVec', second: 'RNVec') -> float:
+        return sum([x*y for x, y in zip(first.coord, second.coord)])
+
+    def __str__(self):
+        return f'({", ".join(map(str, self.coord))})'
+
+
+F = TypeVar('F', bound=Callable[..., sp.Basic])
+
+class SimbolicFunction(IVector['SimbolicFunction']):
+    def __init__(self, func: F, ver: Any):
+        super().__init__()
+        self.func = func
+        self.ver = ver
+
+    def __mul__(self, scalar: float) -> 'SimbolicFunction':
+        return scalar*self.func
+
+    def __add__(self, other: 'SimbolicFunction') -> 'SimbolicFunction':
+        return self.func+other.func
+
+    @staticmethod
+    def inner(first: 'SimbolicFunction', second: 'SimbolicFunction') -> float:
+        print(first.func, second.func, sp.integrate((first.func-second.func)**2, (first.ver, 0, 1)))
+        return sp.integrate(first.func*second.func, (first.ver, 0, 1))
+
+
+def main():
+    a = RNVec(-1, 3, 1, 1)
+    b = RNVec(6, -8, -2, -4)
+    c = RNVec(6, 3, 6, -3)
+    #print(a)
+    #print(b)
+    #print(c)
+    W = FiniteVectorSpace(a, b, c)
+    #print('\n'.join([str(vec) for vec in W.get_orthonormal_base()]))
+    x = sp.symbols('x')
+    print((x-x+1)-(x-x+1))
+    base = (
+        SimbolicFunction(x-x+1, x),
+        SimbolicFunction(x, x),
+        SimbolicFunction(x**2, x),
+    )
+    W = FiniteVectorSpace(*base)
+    print('\n'.join([str(vec) for vec in W.get_orthonormal_base()]))
+
+if __name__ == '__main__':
+    main()
